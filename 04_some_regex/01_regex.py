01a91f299e43527775ed20879333380efcdf1634
Bart Dorlandt <bart@bamweb.nl>
2025-07-20 17:32:19 +0200

    Added regex

---
+++ 04_some_regex/01_regex.py
@@ -0,0 +1,23 @@
+import re
+from typing import Annotated
+
+from pydantic import AfterValidator
+
+
+def port_validator(value: str) -> str:
+    port_re = re.compile(r"^(xe|ge|et)-\d+/\d+/\d+(:\d+)?$")
+
+    if not isinstance(value, str):
+        raise TypeError("Port name must be a string")
+    elif value == "":
+        raise ValueError("Invalid port name")
+    elif not port_re.match(value):
+        raise ValueError(f"Invalid port name: {value}")
+
+    return value
+
+
+Port = Annotated[
+    str,
+    AfterValidator(port_validator),
+]
